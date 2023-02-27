96eba3cfd72dc89f0746370dface3384fa0b7c3d
Tom Keane <tomkeane282@gmail.com>
2023-02-27 11:50:16 +0000

    a

---
+++ java/src/ie/tudublin/Audio2.java
@@ -0,0 +1,73 @@
+package ie.tudublin;
+
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+import processing.core.PApplet;
+
+public class Audio2 extends PApplet
+{
+    Minim minim;
+    AudioPlayer ap;
+    AudioInput ai;
+    AudioBuffer ab;
+
+    int mode = 0;
+
+    float y = 0;
+    float smoothedY = 0;
+    float smoothedAmplitude = 0;
+
+    public void keyPressed() {
+		if (key >= '0' && key <= '9') {
+			mode = key - '0';
+		}
+		if (keyCode == ' ') {
+            if (ap.isPlaying()) {
+                ap.pause();
+            } else {
+                ap.rewind();
+                ap.play();
+            }
+        }
+	}
+
+    public void settings()
+    {
+        size(1024, 1000, P3D);
+        //fullScreen(P3D, SPAN);
+    }
+
+    public void setup()
+    {
+        minim = new Minim(this);
+        // Uncomment this to use the microphone
+        // ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
+        // ab = ai.mix; 
+
+        // And comment the next two lines out
+        ab = ap.mix;
+        colorMode(HSB);
+
+        background(0);
+        y = height / 2;
+        smoothedY = y;        
+    }
+
+    float off = 0;
+
+    float lerpedBuffer[] = new float[1024];
+
+    public void draw()
+    {
+             
+        strokeWeight(2);
+		stroke(50, 0, 255);
+		
+		line(10,10,100,100);
+
+
+
+    }        
+}
