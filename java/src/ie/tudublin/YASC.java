a6d3d4d8dfa1d0de7defd9db0538c0b18e6906e4
Tom Keane <tomkeane282@gmail.com>
2023-03-20 11:50:34 +0000

    a

--- java/src/ie/tudublin/YASC.java
+++ java/src/ie/tudublin/YASC.java
@@ -10,19 +10,18 @@ public class YASC extends PApplet
 	public void settings()
 	{
 		size(500, 500);
+		
 	}
 
 	public void setup() {
-
-		ship = new Ship(width / 2, height / 2, size:50, c:70, this);
+		ship = new Ship(width / 2, height / 2, 50, 70, this);
 		colorMode(HSB);
-		
 	}
 
 	
 	
 	public void draw()
 	{	
-		
+		ship.render();
 	}
 }
